e65a8e22c24111ce6ee1a368a27a0394cab95bfa
Maria Luiza Soares Suhadolnik <maria.luiza.suhadolnik@gmail.com>
2022-11-01 10:49:02 -0300

    resolução do requisito 2

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>Página Carteira</div>;
+    // return <div>TrybeWallet</div>;
+    return (
+      <div className="wallet-page">
+        <Header />
+      </div>
+    );
   }
 }
 
